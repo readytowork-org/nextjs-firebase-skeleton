984484119c9aefafce9116bc528d704cf1b25e8d
Bishal Bhattarai <Bishal>
2022-08-22 23:40:24 +0545

    feat: server side

---
+++ src/firebase.ts
@@ -0,0 +1,22 @@
+import { getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+let firebaseApp = null as any;
+if (!getApps.length) {
+   firebaseApp = initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  });
+}
+
+// Initialize Firebase
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+export { auth, db };
---
+++ src/services/jobs.ts
@@ -0,0 +1,24 @@
+import { collection, getDocs, getDoc, doc } from "firebase/firestore";
+
+import { db } from "../firebase";
+
+export interface JobsProps {
+    id:string;
+    title:string;
+    description: string;
+    category:string;
+}
+
+export const getJobsList = async (): Promise<JobsProps[]> => {
+    const querySnapshot = await getDocs(collection(db, "Jobs"));
+    const jobList = [];
+    querySnapshot.docs.forEach((doc)=> { 
+        jobList.push({ ...doc.data(), id: doc.id });
+    }) as any;
+    return jobList;
+};
+
+export const getJobsDetail = async (id:string): Promise<JobsProps> => {
+    const snap = await getDoc(doc(db, "Jobs", id));
+    return snap.data() as any;
+};
