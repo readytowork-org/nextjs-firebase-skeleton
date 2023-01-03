1faafbc4b558dcf06427232ea6d3871238c298a8
Bishal Bhattarai <Bishal>
2023-01-03 22:20:48 +0545

    fix: server side rendering page css ctx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
     // https://nextjs.org/docs/api-reference/data-fetching/get-initial-props
     // enables server-side rendering in a page and allows you to do initial data population, it means sending the page with the data already populated from the server. This is especially useful for SEO.
-    const initialProps = await Document.getInitialProps(ctx); //
-    return { ...initialProps };
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx); //
+      return { ...initialProps };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
